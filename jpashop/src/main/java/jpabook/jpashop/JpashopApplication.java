caccb9730427e4a1bb6585cfa9ce6937587450cf
chocochip <chocochip@MacBook-Pro.local>
2023-01-08 02:24:39 +0900

    Fix: Lombok Error

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+		System.out.println(data + " Spring");
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
