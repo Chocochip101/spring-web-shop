be1f6df9c4ce95f5e8358f366020979cc997c836
Chocochip101 <fingercut3822@gmail.com>
2023-01-10 01:39:09 +0900

    Feat: Add Member and Repository Find and Save

--- jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -9,11 +9,13 @@ public class MemberRepository {
     @PersistenceContext
     private EntityManager em;
 
+
 //    Command와 Query를 분리해라.
     public Long save(Member member){
         em.persist(member);
         return member.getId();
     }
+
 
     public Member find(Long id){
         return em.find(Member.class, id);
