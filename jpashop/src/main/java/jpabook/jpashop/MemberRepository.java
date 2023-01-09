0c39ddcb0a0e4faa1cfd74b5371ebf95f5b46225
chocochip <chocochip@MacBook-Pro.local>
2023-01-10 01:36:47 +0900

    Feat: Add Member and Repository Find and Save

---
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+//    Command와 Query를 분리해라.
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+}
