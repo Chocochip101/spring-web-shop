a80e8cce2b5fb9b9f60616b9c45cbc68ca71010e
Chocochip101 <fingercut3822@gmail.com>
2023-01-12 10:51:21 +0900

    Feat: Add item, address, delivery, order entities

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+    @OneToMany(mappedBy = "member") // Order 테이블에 있는 member 변수
+    private List<Order> orders = new ArrayList<>();
+}
