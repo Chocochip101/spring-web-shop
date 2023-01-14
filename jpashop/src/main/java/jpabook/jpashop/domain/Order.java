b3116a980fc579acccba80ffcbf0a022c3a30152
Chocochip101 <fingercut3822@gmail.com>
2023-01-14 19:40:54 +0900

    Refactor: 양방향 관계의 편의 메서드 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -19,13 +19,29 @@ public class Order {
     @JoinColumn(name = "member_id") // 와래 키 이름 설정, 매핑을정무엇으로 할지 결정
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate;
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상 [ORDER, CANCEL]
+
+    //==연관관계 편의 메서드 ==//
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
