05656a499fd753eafac059a61ebd112e18aecfe4
Chocochip101 <fingercut3822@gmail.com>
2023-01-19 16:00:06 +0900

    Feat: Order Constructive method

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -44,4 +44,16 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+    //==생성 메서드 ==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem: orderItems){
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
 }
