22b1b685148255b3dc00bc05be27470125b5af31
Chocochip101 <fingercut3822@gmail.com>
2023-01-19 09:15:25 +0900

    Feat: Add Item repository

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+    private final EntityManager em;
+
+    public void save(Item item){
+        // persist할때까지 id가 없으므로 새로운 item
+        if(item.getId() == null){
+            em.persist(item);
+        }
+        // 이미 존재하므로 업데이트
+        else{
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id){
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
