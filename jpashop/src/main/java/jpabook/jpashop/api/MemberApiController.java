dfc3501c7dcd1bcdbd8be4d1faf8dd9b4fbb174e
Chocochip101 <fingercut3822@gmail.com>
2023-01-31 17:46:46 +0900

    Hotfix: Annotation Err

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -28,7 +28,7 @@ public class MemberApiController {
 
 
     @PutMapping("/api/v2/members/{id}")
-    public UpdateMemberResponse updateMemberV2(@@PathVariable("id") Long id,@RequestBody @Valid UpdateMemberRequest request) {
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,@RequestBody @Valid UpdateMemberRequest request) {
         memberService.update(id, request.getName());
         Member findMember = memberService.findOne(id);
         return new UpdateMemberResponse(findMember.getId(), findMember.getName());
