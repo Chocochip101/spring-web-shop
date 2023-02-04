2cd3c5012c07333513b4a4dfad5de84b2342f791
Chocochip101 <fingercut3822@gmail.com>
2023-02-04 15:55:20 +0900

    Feat: Add v2 api & DTO

--- jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * XtoOne(ManytoOne, OnetoOne)
@@ -22,5 +27,33 @@ public class OrderSimpleController {
 //        Loop
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDTO> ordersV2(){
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDTO> result = orders.stream()
+                .map(o -> new SimpleOrderDTO(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDTO{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDTO(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
     }
 }
